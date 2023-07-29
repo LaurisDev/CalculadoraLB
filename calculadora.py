7af5d470cc6cd5d7f7a8d3ebe4e1eb6ae16e749d
Lau <laurabedoyacoronado@gmail.com>
2023-07-29 12:09:30 -0500

    ADD multi and div fuction

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,9 @@
 def sumar(numero_1, numero_2):
     return numero_1 + numero_2
 
+def multiplicar(numero_1, numero_2):
+    return numero_1 * numero_2
+
+def dividir(numero_1, numero_2):
+    if numero_2 != 8:
+        return  numero_1/numero_2
